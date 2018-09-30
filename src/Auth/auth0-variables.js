d4bde35e1f2b519096aea24411bad20a011e99ed
CodHeK <gaganganapathyas@yahoo.in>
2018-09-30 19:12:39 +0530

    callback url changed

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'codhek.auth0.com',
   clientId: 'CA6e7EkF1oMRvWSo0et1exQT9PDT7nzc',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://todos-hasura.herokuapp.com/callback'
 }
